bad0587e1b1e958377fd5ec186cc6a95ed71c991
Reinhard <dijkstraalgorithm07@gmail.com>
2026-06-08 11:30:23 +0200

    CMake Setup
    Setup Cmake setup . Makefile isnt required anymore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CppMatrixes
 My Cpp Matrixes file
+
+# JUST SAVE THE FILE OR RUN THIS COMMAND TO GENERATE BUILD FOLDER
+# cmake -S . -B Build -G "MINGW Makefiles"
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <string>
 
-#include "inc/Matrix.h"
+#include "Matrix.h"
 
 int main()
 {
-    Matrix<double> M;
+    Matrix<double> M ;
     std::vector<std::vector<std::string>> data;
 
     std::vector<std::vector<double>> a = {
         {4, 3, 8},
         {6, 2, 5},
-        {1, 5, 9}};
+        {1, 5, 8}}; 
 
     std::vector<std::vector<double>> c ; 
     std::vector<std::vector<double>> b = M.Inverse(a) ;
-    
+
+    M.printMatrixes(a) ;
+    M.printMatrixes(b) ; 
+
+    M.printMatrixes(M.MultiplyMatrix(a,b)) ; 
+
     std::cout << "Program Ran to Completion\n";
     system("pause");
-    system("cls");
     return 0;
 }
