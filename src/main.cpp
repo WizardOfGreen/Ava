a279212c750b5d55fe54535ef50c380c85074271
Oriax <dijstraalgorithm07@gmail.com>
2026-08-17 13:52:23 +0200

    Added a testing framework

--- src/main.cpp
+++ src/main.cpp
@@ -403,7 +403,7 @@ int main()
     // Neural Networks :                        
     // =======================================  
     // SingleLayerBP();                         // 1 Layer , 2 Inputs , 2 Output            
-    MultipleHiddenLayers();                  // 3 Layers , 3 Inputs , 2 Outputs          
+    MultipleHiddenLayers();                     // 3 Layers , 3 Inputs , 2 Outputs          
     // XORTraining();                           // 1 Layers , 2 Inputs ,1 Output.           
     // XORSolved() ;                            // Solved Problem To Test Feed Forwarding.  
     // FeedForwardTest();                       // Tests Feed Forward Pass                  
@@ -419,7 +419,6 @@ int main()
     // =======================================  
 
     std::cout << "Program Ran Successfully\n";
-    system("pause");
-
+    
     return 0;
 }
