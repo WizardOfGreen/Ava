63c2dcef96172b1294820e0d2310a8cf882cc878
Oriax <dijstraalgorithm07@gmail.com>
2026-08-24 15:10:22 +0200

    StepFuncTest

--- tests/act_stepfunc.cpp
+++ tests/act_stepfunc.cpp
@@ -4,7 +4,31 @@
 
 #include "ActFunctions.h"
 
-int main ()
+int main()
 {
-    return 0 ; 
+    bool failed = false; 
+    std::vector<double> inp = {0.5, -0.5};
+    std::vector<double> out = {0, 0};
+    std::vector<double> res = {1, 0};
+
+    ActivationFunction * stepfunc = new StepFunc ; 
+
+    for ( int i = 0 ; i < 2 ; i++)
+    {
+        out[i] = stepfunc->activate(inp[i]) ; 
+
+        if ( compare(out[i] , res[i]))
+        {
+            std::cout << "Step Function " << (i+1) << " Failed!\n" ;
+            std::cout << "Result : " << out[i] << " True Value : " << res[i] << std::endl ;
+            failed = true ; 
+        }
+        else
+        {
+            std::cout << "Step Function " << (i+1) << " Passed!\n" ;
+        }
+
+    }
+
+    return failed;
 }
