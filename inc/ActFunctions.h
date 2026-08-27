9d33e4dab4b65eac72b0911813cea2f144023f1d
WizardOfGreen <dijkstraalgorithm07@gmail.com>
2026-08-27 13:21:47 +0200

    Added Some documentations

--- inc/ActFunctions.h
+++ inc/ActFunctions.h
@@ -7,12 +7,22 @@ class Sigmoid : public ActivationFunction
 {
 public:
     // σ(x) = 1 / (1 + e^-x)
+    /**
+     * Standard Sigmoid function
+     * σ(x) = 1 / (1 + e^-x)
+     * @param x Value to be activated
+     */
     double activate(double x)
     {
         return 1 / (1 + pow(e, (x * -1)));
     }
 
     // σ'(x) = σ(x) * ( 1 - σ(x) ) ;  
+    /**
+     * Activated Derivative of Sigmoid
+     * @param a Will be converted using sigmoid and used in the derivative.
+     * σ'(x) = σ(x) * ( 1 - σ(x) )
+     */
     double activateDerivative(double a)
     {
         double x = activate(a) ;
@@ -20,6 +30,11 @@ public:
         return res;
     }
     // activation not required for derivative
+    /**
+     * Derivative of the sigmoid function where parameter a is not 
+     * activated by the sigmoid
+     * 
+     */
     double pre_activation_derivative(double a)
     {
         double x = (a) ;
@@ -31,17 +46,26 @@ public:
 class ReLU : public ActivationFunction
 {
 public:
+    /**
+     * Standard Relu formula
+     */
     double activate(double x)
     {
-        return ((x >= 0.0) ? x : 0.0 );
+        return ((x > 0.0) ? x : 0.0 );
     }
 
+    /**
+     * Derivative of Relu , Activates between 1 and 0
+     */
     double activateDerivative(double a)
     {
         double x = a;
         return x > 0.0 ? 1.0 : 0.0;
     }
 
+    /**
+     * Same as activateDerivative
+     */
     double pre_activation_derivative(double x) 
     {
         return (x > 0.0) ? 1.0 : 0.0 ; 
@@ -49,20 +73,32 @@ public:
 
 };
 
+
 class SoftPlus : public ActivationFunction
 {
 public:
+/**
+ * Standard Softplus function
+ * f(x) = ln(1 + e^x) 
+ */
     double activate(double x)
     {
         return std::log(1 + pow(e, x));
     }
 
+    /**
+     * Derivative of Softplus
+     * f'(x) = 1 / (1 + e^-x) ; 
+     */
     double activateDerivative(double a)
     {
         double res = 1 / (1 + pow(e, (-1 * a)));
         return res;
     }
 
+    /**
+     * Same as activate Derivative
+     */
     double pre_activation_derivative(double x )
     {
         double res = 1 - ( pow ( e , -1 * x )) ; 
